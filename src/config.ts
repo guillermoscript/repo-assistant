ad25b651a46aac3619e95fb1263f0c7e534014d5
guillermoscript <arepayquezo@gmail.com>
2024-01-04 18:20:05 -0400

    Update botConfig in config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,12 @@
-export const botConfig = {
+import { ChatCompletionCreateParamsBase } from "openai/resources/chat/completions"
+
+type Config = {
+    similarityThreshold: number,
+    gptModel: ChatCompletionCreateParamsBase['model']
+}
+
+export const botConfig: Config = {
     // Define a similarity threshold
     similarityThreshold: 0.8,
-
+    gptModel: "gpt-4-1106-preview"
 }
