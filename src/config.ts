b513e236b8cdbaebf854b14af6fce0426cbf0480
guillermoscript <arepayquezo@gmail.com>
2024-02-11 14:01:21 -0400

    Update botConfig in config.ts and add pull request event listener in index.ts

--- src/config.ts
+++ src/config.ts
@@ -3,10 +3,12 @@ import { ChatCompletionCreateParamsBase } from "openai/resources/chat/completion
 type Config = {
     similarityThreshold: number,
     gptModel: ChatCompletionCreateParamsBase['model']
+    emmbeddingModel: string
 }
 
 export const botConfig: Config = {
     // Define a similarity threshold
     similarityThreshold: 0.8,
-    gptModel: "gpt-4-1106-preview"
+    gptModel: "gpt-4-turbo-preview",
+    emmbeddingModel: 'text-embedding-3-small' // issues with the model
 }
